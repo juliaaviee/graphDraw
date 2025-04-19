fe99cb6f4d0c56dcde295b71c62ef7d48a80c6d9
Lightjv23 <132280489+Lightjv23@users.noreply.github.com>
2025-04-19 02:08:38 -0300

    Update structures.h

--- structures.h
+++ structures.h
@@ -10,21 +10,41 @@
 #include <QGraphicsLineItem>
 #include <QLineF>
 #include <QPointF>
-
+#include <QPolygonF>
 
 class Node;
 
-class Edge : public QGraphicsLineItem {
+class Edge : public QObject, public QGraphicsLineItem {
+    Q_OBJECT
 public:
-    Edge(Node* sourceNode, Node* destNode);
+    Edge(Node* sourceNode, Node* destNode, int w=1, int n=0);
+    QVariant itemChange(GraphicsItemChange change, const QVariant &value) override;
     Node* getSource();
     Node* getDest();
     void updatePos();
     void highlightEdge();
     void clearEdge();
+    void setWeight(int w);
+    void changeNudge(int n);
+    int getWeight();
+signals:
+    void selected(Edge* e);
+protected:
+    void paint(QPainter *painter, const QStyleOptionGraphicsItem *option, QWidget *widget) override;
+    QRectF boundingRect() const override;
+    QPainterPath shape() const override;
 private:
+    QGraphicsTextItem* label;
     Node* source;
     Node* dest;
+    int weight;
+    int nudge; // The nudge factor is what helps us make the edges parallel to each other, in case it's a 2-way connection
+};
+
+struct Route {
+    QString visual;
+    QList<Edge*> edges;
+    int totalCost=0;
 };
 
 class Node : public QObject, public QGraphicsEllipseItem {
