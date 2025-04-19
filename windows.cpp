d39a9fee8000d3429436cc02273305f743aa56de
Lightjv23 <132280489+Lightjv23@users.noreply.github.com>
2025-04-19 02:10:09 -0300

    Update windows.cpp

--- windows.cpp
+++ windows.cpp
@@ -10,18 +10,28 @@ MainWindow::MainWindow(QWidget *parent)
     , main_ui(new Ui::MainWindow)
 {
     main_ui->setupUi(this);
-    scene = new QGraphicsScene(this);
-    main_ui->graphicsView->setScene(scene);
     setWindowTitle("graphDraw");
     setFixedSize(799,584);
+
+    scene = new QGraphicsScene(this);
+    main_ui->graphicsView->setScene(scene);
+
     name = main_ui->nodeName;
     newname = main_ui->newName;
     src = main_ui->src;
     dst = main_ui->dst;
 
+    cost = main_ui->sortCost;
+    length = main_ui->sortLen;
+
     changeName = main_ui->changeName;
     showRoutes = main_ui->routes;
     showMatrix = main_ui->showMatrix;
+
+    QButtonGroup* sorting = new QButtonGroup(this);
+    sorting->addButton(cost);
+    sorting->addButton(length);
+    length->setChecked(true);
 
     connectionS = new QShortcut(QKeySequence("Ctrl+Z"), this);
     MainWindow::connect(connectionS, &QShortcut::activated, this, [this]() {
@@ -79,15 +89,22 @@ MainWindow::MainWindow(QWidget *parent)
             qDebug() << "Invalid label(s)";
             return;
         }
-        QList<QPair<QString, QList<Edge*>>> routes;
-        traversal(st,f, "", routes,vis,b);
+        QList<Route> routes;
+        Route r;
+        traversal(st,f, r, 0, routes,vis,b);
         if(routes.empty()) {
             qDebug() << "No possible routes from" << s << "to" << d;
             return;
         }
-        std::sort(routes.begin(), routes.end(), [](const QPair<QString, QList<Edge*>> &a, const QPair<QString, QList<Edge*>> &b) {
-            return a.first.size() < b.first.size();  // sort by length
-        });
+        if(length->isChecked()) {
+            std::sort(routes.begin(), routes.end(), [](const Route &a, const Route &b) {
+                return a.visual.size() < b.visual.size();  // sort by length
+            });
+        } else {
+            std::sort(routes.begin(), routes.end(), [](const Route &a, const Route &b) {
+                return a.totalCost < b.totalCost;  // sort by cost
+            });
+        }
         if(routeWindow) routeWindow->close();
         routeWindow = new RouteWindow(routes);
         routeWindow->show();
@@ -101,7 +118,7 @@ MainWindow::MainWindow(QWidget *parent)
         QList<QPair<QString, QString>> matrix;
         int cnt{};
         for(Node* no: nodes) {
-            //Printing out columns while mapping labels to index
+            //Mapping labels to index
             idx[no->getLabel()] = cnt;
             cnt++;
         }
@@ -130,36 +147,35 @@ MainWindow::~MainWindow()
     delete main_ui;
 }
 
-void MainWindow::traversal(Node *c, Node *d, QString r, QList<QPair<QString, QList<Edge*>>> &rs, std::unordered_map<Node*, bool> v, std::unordered_map<QString, Node*> b) {
+void MainWindow::traversal(Node* c, Node* d, Route r, int sum, QList<Route>& rs, std::unordered_map<Node*, bool> v, std::unordered_map<QString, Node*> &b) {
     if(v[c]) return; // If it has been visited already, return
     if(c==d) {
-        // If we have arrived at our destination, concatenate it to the end of the route and append it to routes, then return
-        r += c->getLabel();
-        QPair<QString, QList<Edge*>> p = qMakePair(r, backtrack(r, b));
-        rs.append(p);
+        // If we have arrived at our destination, concatenate it to the end of the string and append it to routes, then return
+        r.visual += c->getLabel();
+        r.totalCost = sum;
+        r.edges = backtrack(r.visual,b);
+        rs.append(r);
         return;
     }
     v[c] = true; // Set the current node as visited
-    r += c->getLabel() + QString::fromStdString("->");
+    r.visual += c->getLabel() + QString::fromStdString("->");
     QList<Edge*> es = c->getCons();
     for(Edge* e: es) {
-        Node* s = e->getSource();
         Node* ds = e->getDest();
-        if(c!=s) traversal(s,d,r,rs,v,b);
-        else traversal(ds,d,r,rs,v,b);
+        if(c!=ds) traversal(ds,d,r,sum+e->getWeight(),rs,v,b); // If the destination of the current edge isn't the current node, visit it
     }
 }
 
 //The backtrack function was created to ensure we get the correct edges that are to be highlighted, since we had a few issues with traversal.
 //It is called when we find our destination in the traversal recursion
-QList<Edge*> MainWindow::backtrack(const QString &r, std::unordered_map<QString, Node *> b) {
+QList<Edge*> MainWindow::backtrack(const QString &r, std::unordered_map<QString, Node *> &b) {
     QList<Edge*> ans;
     QStringList t = r.split("->");
     int s = t.size()-1;
     for(int l = 0;l<s;l++) {
         QList<Edge*> re = b[t[l]]->getCons();
         for(auto e: re) {
-            if((b[t[l]] == e->getSource() && b[t[l+1]] == e->getDest()) || (b[t[l]] == e->getDest() && b[t[l+1]] == e->getSource())) {
+            if(b[t[l]] == e->getSource() && b[t[l+1]] == e->getDest()) {
                 ans.append(e);
                 break;
             }
@@ -204,22 +220,44 @@ void MainWindow::nodeInteraction(Node *node) {
     if(canConnect) {
         if(!tmp) tmp = node;
         else if(tmp != node) {
-            QList<Edge*> edges = node->getCons();
-            for(Edge* n: edges) {
-                Node* s = n->getSource();
-                Node* d = n->getDest();
-                if((tmp == s && node == d) || (node == s && tmp == d)) {
+            QList<Edge*> edges = tmp->getCons();
+            for(Edge* e: edges) {
+                //This loop ensures this isn't a connection we've made before
+                if(tmp == e->getSource() && node == e->getDest()) {
                     tmp = nullptr;
                     return;
                 }
             }
+
+            QList<Edge*> edges1 = node->getCons();
+            for(Edge* e: edges1) {
+                //If we find the counterpart to the connection about to be made, make them parallel through the nudge factor
+                if(node == e->getSource() && tmp == e->getDest()) {
+                   e->changeNudge(1);
+                   e->updatePos();
+                   Edge* ed = new Edge(tmp, node,1,1);
+                   MainWindow::connect(ed, &Edge::selected, this, &MainWindow::edgeInteraction);
+                   tmp->addEdge(ed);
+                   node->addEdge(ed);
+                   scene->addItem(ed);
+                   tmp=nullptr;
+                   if(edgeWindow) edgeWindow->close();
+                   edgeWindow = new EdgeWindow(ed);
+                   edgeWindow->show();
+                   return;
+                }
+            }
             Edge* e = new Edge(tmp, node);
+            MainWindow::connect(e, &Edge::selected, this, &MainWindow::edgeInteraction);
             tmp->addEdge(e);
             node->addEdge(e);
             scene->addItem(e);
             tmp = nullptr;
+            if(edgeWindow) edgeWindow->close();
+            edgeWindow = new EdgeWindow(e);
+            edgeWindow->show();
         }
-    }
+    } else tmp = nullptr;
     if(canDelete) {
         QList<Edge*> edges = node->getCons();
         for(Edge* e: edges) {
@@ -251,6 +289,28 @@ void MainWindow::nodeInteraction(Node *node) {
     }
 }
 
+void MainWindow::edgeInteraction(Edge *e) {
+    if(canDelete) {
+        Node* s = e->getSource();
+        Node* ds = e->getDest();
+        QList<Edge*> check = s->getCons();
+        for(Edge* ch: check) {
+            //If there's a counterpart to the edge to be deleted, center it
+            if(ch->getSource()==ds&& ch->getDest()==s) {
+                ch->changeNudge(0);
+                ch->updatePos();
+                break;
+            }
+        }
+        e->getSource()->removeEdge(e);
+        e->getDest()->removeEdge(e);
+        scene->removeItem(e);
+        return;
+    }
+    if(edgeWindow) edgeWindow->close();
+    edgeWindow = new EdgeWindow(e);
+    edgeWindow->show();
+}
 void MainWindow::on_reset_clicked()
 {
     if(routeWindow) routeWindow->close();
@@ -265,6 +325,7 @@ void MainWindow::closeEvent(QCloseEvent *event) {
     //Ensuring children windows are closed
     if(routeWindow) routeWindow->close();
     if(matrixWindow) matrixWindow->close();
+    if(edgeWindow) edgeWindow->close();
     event->accept();
 }
 MatrixWindow::MatrixWindow(int r, int c, const QList<QPair<QString, QString>> &m, QWidget *parent) : QDialog(parent) {
@@ -325,19 +386,19 @@ void ComboBoxDelegate::setModelData(QWidget *editor, QAbstractItemModel *model, 
     model->setData(index, combo->currentText(), Qt::EditRole);
 }
 
-RouteWindow::RouteWindow(const QList<QPair<QString, QList<Edge*>>> &rs, QWidget *parent) : QDialog(parent) {
+RouteWindow::RouteWindow(const QList<Route> &rs, QWidget *parent) : QDialog(parent) {
     setWindowTitle("Routes: " + QString::number(rs.length()));
     setFixedSize(500,300);
     list = new QListWidget(this);
-    for(auto r: rs) {
+    for(Route r: rs) {
         //Mapping routes to their respective edges while populating list
-        routes[r.first] = r.second;
-        list->addItem(r.first);
+        routes[r.visual] = r.edges;
+        list->addItem(r.visual + " Cost: " + QString::number(r.totalCost));
     }
     list->setFixedSize(500,300);
     connect(list, &QListWidget::itemClicked, this, [this](QListWidgetItem* item) {
         for(Edge* e: tmp) e->clearEdge();
-        QList<Edge*> es = routes[item->text()];
+        QList<Edge*> es = routes[item->text().split(" ")[0]];
         tmp = es;
         for(Edge* e: es) e->highlightEdge();
     });
@@ -347,5 +408,22 @@ void RouteWindow::closeEvent(QCloseEvent *event) {
     //Overriding the close event for the route window so that there are no leftover highlighted routes
     for(auto r: tmp) r->clearEdge();
     event->accept();
+}
+
+EdgeWindow::EdgeWindow(Edge* e, QWidget *parent) : QDialog(parent) {
+    setFixedSize(140,25);
+    setWindowTitle("Insert weight");
+    w = new QLineEdit(this);
+    edge = e;
+    EdgeWindow::connect(w, &QLineEdit::returnPressed, this, [this]() {
+        QString l = w->text();
+        if(l.isEmpty()) {
+            close();
+        }
+        else {
+            edge->setWeight(l.toInt());
+            close();
+        }
+    });
 }
 
