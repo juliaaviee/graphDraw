46be597390f3992ddf115cb740e349ded2a258ce
Lightjv23 <132280489+Lightjv23@users.noreply.github.com>
2025-05-04 00:31:07 -0300

    Update windows.h

--- windows.h
+++ windows.h
@@ -46,7 +46,8 @@ public:
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
     void traversal(Node* c, Node* d, Route r, int sum, QList<Route>& rs, std::unordered_map<Node*, bool> v, std::unordered_map<QString, Node*> &b);
-    QList<Edge*> backtrack(const QString &r, std::unordered_map<QString, Node*> &b);
+    void traversal (Node* c, Node* d, Route r, QList<Route>& rs, std::unordered_map<Node*, bool> v, std::unordered_map<QString, Node*> &b);
+    QList<Edge*> backtrack(const QString &r, std::unordered_map<QString, Node*> &b, bool dir);
 protected:
     void closeEvent(QCloseEvent *event) override;
 private slots:
@@ -55,7 +56,10 @@ private slots:
     void edgeInteraction(Edge* e);
     void matrixResponse(const QModelIndex &topLeft, const QModelIndex &bottomRight);
     void on_reset_clicked();
-    void on_weightV_stateChanged(int arg1);
+    void on_weightV_stateChanged(int b);
+    void on_enWeight_stateChanged(int b);
+    void on_enDirection_stateChanged(int b);
+    void on_showMatrix_clicked();
 
 private:
     Ui::MainWindow *main_ui;
@@ -72,11 +76,14 @@ private:
     QRadioButton* length;
     QCheckBox* weightV;
     QCheckBox* weightP;
+    QComboBox* nodeAmount;
     Node* tmp = nullptr;
     Node* cur = nullptr;
     QList<Node*> nodes;
     bool canConnect=false;
     bool canDelete = false;
+    bool directionEnabled = true;
+    bool weightEnabled = true;
     QShortcut* connectionS;
     QShortcut* deletionS;
     MatrixWindow* matrixWindow=nullptr;
@@ -97,10 +104,11 @@ class MatrixWindow: public QDialog
 {
     Q_OBJECT
 public:
-    MatrixWindow(int r, int c, const QList<QPair<QString, QString>> &m, const std::unordered_map<int, Node*> &nMap, QWidget *parent = nullptr);
+    MatrixWindow(unsigned short r, unsigned short c, const QList<QPair<QString, QString>> &m, const std::unordered_map<int, Node*> &nMap, bool dir, QWidget *parent = nullptr);
     std::unordered_map<int, Node*>& getNodeMap();
     std::unordered_map<std::pair<int,int>, Edge*, PairHash>& getCellCons();
     QStandardItemModel* getModel();
+    void toggleDirection(bool dir);
     void removeCon(Edge* e);
     void addCon(Edge* e);
 private:
@@ -108,6 +116,7 @@ private:
     QStandardItemModel *model;
     std::unordered_map<int, Node*> nodeMap;
     std::unordered_map<std::pair<int,int>, Edge*, PairHash> cellCons;
+    bool directed = true;
 };
 
 class ComboBoxDelegate : public QStyledItemDelegate {
@@ -135,13 +144,14 @@ class RouteWindow : public QDialog
 {
     Q_OBJECT
 public:
-    RouteWindow(const QList<Route> &rs, QWidget *parent = nullptr);
+    RouteWindow(const QList<Route> &rs, bool w, QWidget *parent = nullptr);
 protected:
     void closeEvent(QCloseEvent *event) override;
 private:
     std::unordered_map<QString, QList<Edge*>> routes;
     QList<Edge*> tmp;
     QListWidget *list;
+    bool weighted;
 };
 
 #endif // WINDOWS_H
